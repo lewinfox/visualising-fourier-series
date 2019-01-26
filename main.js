2f86529bca06bc6b2e7f83572fa39e1e22622e05
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2019-01-26 19:53:25 +0000

    Epicycles working properly

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 console.log('main.js connected');
 
 let angle = 0;
+let wave_data = [];
+let slider  = 4;
 
 function setup() {
     let canvas = createCanvas(windowWidth * 0.8, windowHeight * 0.8);
     canvas.parent('canvas-container');
+    // slider = createSlider(1, 10, 1); // Number of circles
     main_x = width / 5;
     main_y = height / 2;
     main_diameter = 200;
 }
 
 function draw() {
-    // resizeCanvas(windowWidth * 0.9, windowHeight * 0.8); 
     background(16);
 
-    // Draw the first circle
-    noFill();
-    stroke(255);
-    ellipse(main_x, main_y, main_diameter);
+    translate(main_x, main_y);
 
-    // Calculate the position of the blob based on `angle`
-    let x = Math.sin(angle) * main_diameter / 2;
-    let y = Math.cos(angle) * main_diameter / 2;
-    translate(main_x, main_y);  // Gets us into the centre of the main circle
-    // Draw a line
-    line(0, 0, x, y)
-    fill(255);
-    ellipse(x, y, 10, 10);
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < slider; i ++) {
+        let prev_x = x;
+        let prev_y = y;
+
+        let n = i * 2 + 1;  // Getting the right progression for the fourier series
+        let radius = 75 * (4 / (n * PI));
+
+        x += radius * cos(n * angle);
+        y += radius * sin(n * angle);
+
+        stroke(255, 100);
+        noFill();
+        ellipse(prev_x, prev_y, radius * 2);
+
+        stroke(255);
+        line(prev_x, prev_y, x, y);
+    }
 
     angle += 0.01;
 }
