5359196f40b9f5bdf0c3be7096f18f3eb51e642a
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2019-01-26 20:01:20 +0000

    Drawing wave

--- main.js
+++ main.js
@@ -25,19 +25,39 @@ function draw() {
         let prev_x = x;
         let prev_y = y;
 
-        let n = i * 2 + 1;  // Getting the right progression for the fourier series
+        let n = i * 2 + 1;  // Getting the right progression for the fourier series (1, 3, 7...)
         let radius = 75 * (4 / (n * PI));
 
         x += radius * cos(n * angle);
         y += radius * sin(n * angle);
 
+        // Draw the circle
         stroke(255, 100);
         noFill();
         ellipse(prev_x, prev_y, radius * 2);
 
+        // Draw the line
         stroke(255);
         line(prev_x, prev_y, x, y);
     }
 
-    angle += 0.01;
+    // Add the new point into the wave array
+    wave_data.unshift(y);
+
+    // Drawing the wave
+    translate(200, 0);
+    line(x - 200, y, 0, wave_data[0]);  // Line joining the tip of the circle to the start of the wave
+    beginShape();
+    noFill();
+    for (let i = 0; i < wave_data.length; i++) {
+        vertex(i, wave_data[i]);
+    }
+    endShape();
+
+    // Trim the wave_data array to stop it getting too large
+    if (wave_data.length > width) {
+        wave_data.pop();
+    }
+
+    angle += 0.03;
 }
